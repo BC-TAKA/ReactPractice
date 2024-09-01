480efa1cd0cdaa26d3823bb46281df64ea0f2427
TAKA-SAN <ytakada0109@gmail.com>
2024-09-01 12:45:39 +0900

    コメント追加

--- api/main.go
+++ api/main.go
@@ -8,16 +8,21 @@ import (
 )
 
 func main() {
+	// インスタンス作成
 	e := echo.New()
 
+	// ミドルウェア設定
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
+	// エンドポイント
 	e.GET("/", hello)
 
+	// ポート番号1323で起動
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
+// 疎通確認用ハンドラー
 func hello(c echo.Context) error {
 	return c.String(http.StatusOK, "Welcome to Golang!")
 }
